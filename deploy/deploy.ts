2144151461dd7f50fb8cf0d3ea0bbe02f11d4234
Napcalx <karlike24@gmail.com>
2024-10-29 02:18:04 +0100

    Updated the Config for the Frontend Interface

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -1,14 +1,12 @@
+// deployment code goes here
 import { deployContract } from "./utils";
 
-
 export default async function () {
-    // Deploying the GeneralPaymaster contract
-    const Paymaster = "GeneralPaymaster";
-    const PaymasterArguments = [];
-    await deployContract(Paymaster, PaymasterArguments);
+  const Paymaster = "GeneralPaymaster";
+  const PaymasterArguments = [];
+  await deployContract(Paymaster, PaymasterArguments);
 
-    // Deploying the zkTune contract
-    const contractArtifactName = "zkTune";
-    const dAppArguments = [];
-    await deployContract(contractArtifactName, dAppArguments);
+  const contractArtifactName = "zkTune";
+  const dAppArguments = [];
+  await deployContract(contractArtifactName, dAppArguments);
 }
